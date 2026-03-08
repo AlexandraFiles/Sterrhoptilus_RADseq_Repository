061a9d1c26062f937fdac9b68fa2408d1c8f0ede
AlexandraFiles <alexandrafiles@ku.edu>
2026-03-07 18:32:35 -0600

    Added Admixture K=5

--- Admixture/Sterrhoptilus_VisualizingAdmixture_AllSpecies.R
+++ Admixture/Sterrhoptilus_VisualizingAdmixture_AllSpecies.R
@@ -78,6 +78,16 @@ runs[[4]]<-runs[[4]][with(runs[[4]], order(V1, -V3, -V4)),]
 #runs[[4]]<-round(runs[[4]], digits = 5)
 #runs[[4]]<-runs[[4]][with(runs[[4]], order(V2, -V3, -V4)),]
 
+#to sort run 5, sort by capitalis ancestry (V3), nigrocapitatus ancestry (V5), affinis ancestry (V1), then by dennistouni north ancestry ascending (V2)
+runs[[5]]<-round(runs[[5]], digits = 5)
+runs[[5]]<-runs[[5]][with(runs[[5]], order(V3, V5, V1, -V2)),]
+#for singleton runs have to manually fix one
+#correct_order <- c(c(22:36), "39", "19", "16", "10", c(12:15), "17", "18", "20", "21", "37", "38",
+#                   "11", "45", c(42:44), "40", "41", "46", "47", c(1:9))
+#correct_order_indices <- match(correct_order, rownames(runs[[5]]))
+#runs[[5]] <- runs[[5]][correct_order_indices, ]
+#runs[[5]]<-round(runs[[5]], digits = 5)
+
 #set margins
 par(mar = c(0.5, 0.5, 1, 1), oma = c(6, 0.5, 1, 1))
 par(mfrow=c(1,1))
@@ -86,8 +96,9 @@ par(mfrow=c(1,1))
 barplot(t(as.matrix(runs[[2]])), col=c("#f4b71c","#b22b04"), ylab="Ancestry", border="black" , names.arg = F)
 barplot(t(as.matrix(runs[[3]])), col=c("#242424","#f4b71c","#b22b04"), ylab="Ancestry", border="black" , names.arg = F)
 barplot(t(as.matrix(runs[[4]])), col=c("#b22b04","#242424","#f4b71c","#6f6f6f"), ylab="Ancestry", border="black", names.arg = F)
+barplot(t(as.matrix(runs[[5]])), col=c("#6f6f6f","#f4b71c","#b22b04","#b79300", "#242424"), ylab="Ancestry", border="black", names.arg = F)
 #for singleton runs, use the following order for correct colors
-#barplot(t(as.matrix(runs[[4]])), col=c("#242424", "#d83406","#f4b71c","#6f6f6f"), ylab="Ancestry", border="black", names.arg = F)
+#barplot(t(as.matrix(runs[[4]])), col=c("#242424", "#b22b04","#f4b71c","#6f6f6f"), ylab="Ancestry", border="black", names.arg = F)
 
 #add lines for labels
 segments(x0 = 0.2, y0 = -0.03,x1 = 35.8, y1 = -0.03, xpd = NA, lwd = 3, col = "#f4b71c")
@@ -102,13 +113,14 @@ text(x = 46.5, y = -0.158, labels = "S. nigrocapitatus", xpd = NA, srt = -45)
 
 #Or, to display together:
 par(mar = c(0, 3, 0.5, 0.5), oma = c(6, 0.25, 0, 0))
-par(mfrow=c(2,1))
+par(mfrow=c(3,1))
 
 #plot barplots using the sorted runs
 barplot(t(as.matrix(runs[[3]])), col=c("#242424","#f4b71c","#b22b04"), ylab="Ancestry", border="black" , names.arg = F)
 barplot(t(as.matrix(runs[[4]])), col=c("#b22b04","#242424","#f4b71c","#6f6f6f"), ylab="Ancestry", border="black", names.arg = F)
+barplot(t(as.matrix(runs[[5]])), col=c("#6f6f6f","#f4b71c","#b22b04","#b79300", "#242424"), ylab="Ancestry", border="black", names.arg = F)
 #for singleton runs, use the following order for correct colors
-#barplot(t(as.matrix(runs[[4]])), col=c("#242424", "#d83406","#f4b71c","#6f6f6f"), ylab="Ancestry", border="black", names.arg = F)
+#barplot(t(as.matrix(runs[[4]])), col=c("#242424", "#b22b04","#f4b71c","#6f6f6f"), ylab="Ancestry", border="black", names.arg = F)
 
 #add lines for labels
 segments(x0 = 0.2, y0 = -0.03,x1 = 35.8, y1 = -0.03, xpd = NA, lwd = 3, col = "#f4b71c")
